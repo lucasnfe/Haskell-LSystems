d1aef811c4e2d9918e1d7b47232399b378476d58
Lucas N. Ferreira <lucasnfe@gmail.com>
2016-02-27 16:48:43 -0800

    Basic LSystems implementation

--- lsystems.hs
+++ lsystems.hs
@@ -2,6 +2,13 @@
 import System.Environment
 import Graphics.Gloss
 
+-- data types
+type Rule     = (Char, String)
+type LSystem  = (Int, Float, String, [Rule])
+
+consts :: String
+consts = ['+', '-']
+
 getGrammarLine :: String -> [String]
 getGrammarLine s = lines s
 
@@ -10,13 +17,26 @@ parseGrammarFile fileName = do
   text <- readFile fileName
   return (getGrammarLine text)
 
+execRule :: Char -> Rule -> String
+execRule x (pred, sec) = if x == pred then sec else []
+
+execAllRules :: [Rule] -> Char -> String
+execAllRules rules x = if x `elem` consts then [x] else execRule x =<< rules
+
+replace :: LSystem -> String
+replace (_ ,_ ,s ,rs) = execAllRules rs =<< s
+
+expandAxiom :: LSystem -> String
+expandAxiom (0, _ , axiom, _) = axiom
+expandAxiom (i, angle, axiom, rules) = expandAxiom (i-1, angle, replace (i, angle, axiom, rules), rules)
+
 -- our program counts the number of spaces in input.txt
 main :: IO ()
 main = do
     c <- getArgs
     n <- parseGrammarFile (head c)
     print n
-    
+
     -- Render the grammar
     display
         (InWindow "Gloss Line"
