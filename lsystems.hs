f1b749a282280d9693b90517f69e70ef774d08d7
Lucas N. Ferreira <lucasnfe@gmail.com>
2016-02-27 22:51:17 -0800

    Expanding L-System described by a grammar file

--- lsystems.hs
+++ lsystems.hs
@@ -1,6 +1,7 @@
 -- Haskell L-System
 import System.Environment
 import Graphics.Gloss
+import Data.List.Split
 
 -- data types
 type Rule     = (Char, String)
@@ -8,6 +9,46 @@ type LSystem  = (Int, Float, String, [Rule])
 
 consts :: String
 consts = ['+', '-']
+
+-- Parse L-Systems recursive step, it's the first (0) element in the list
+parseStept :: [String] -> IO Int
+parseStept grammarData = return (read (grammarData !! 0) :: Int)
+
+-- Parse L-Systems angle, it's the second (1) element in the list
+parseAngle :: [String] -> IO Float
+parseAngle grammarData = return (read (grammarData !! 1) :: Float)
+
+-- Parse L-Systems axiom, it's the third (2) element in the list
+parseAxiom :: [String] -> IO String
+parseAxiom grammarData = return (grammarData !! 2)
+
+-- Parse L-Systems rules, they are all the elements after the second
+breakRule :: String -> [String]
+breakRule rule = splitOn "=" rule
+
+createRule :: String -> Rule
+createRule rule = (((breakRule rule) !! 0) !! 0, (breakRule rule) !! 1)
+
+parse :: [String] -> [Rule]
+parse [] = []
+parse (x:y) = [createRule x] ++ (parse y)
+
+getRules :: [String] -> IO [String]
+getRules grammarData = return (take ((length grammarData) - 3) (reverse grammarData))
+
+parseRules :: [String] -> IO [Rule]
+parseRules grammarData = do
+    rulesData <- getRules grammarData
+    return (parse rulesData)
+
+-- Parse L-Systems grammar, expects a list of strings and return an L-System
+parseGrammarData :: [String] -> IO LSystem
+parseGrammarData grammarData = do
+    steps <- parseStept grammarData
+    angle <- parseAngle grammarData
+    axiom <- parseAxiom grammarData
+    rules <- parseRules grammarData
+    return (steps, angle, axiom, rules)
 
 getGrammarLine :: String -> [String]
 getGrammarLine s = lines s
@@ -18,33 +59,38 @@ parseGrammarFile fileName = do
   return (getGrammarLine text)
 
 execRule :: Char -> Rule -> String
-execRule x (pred, sec) = if x == pred then sec else []
+execRule x (pre, sec) = if x == pre then sec else []
 
 execAllRules :: [Rule] -> Char -> String
 execAllRules rules x = if x `elem` consts then [x] else execRule x =<< rules
 
 replace :: LSystem -> String
-replace (_ ,_ ,s ,rs) = execAllRules rs =<< s
+replace (_, _, s, rs) = execAllRules rs =<< s
 
 expandAxiom :: LSystem -> String
 expandAxiom (0, _ , axiom, _) = axiom
-expandAxiom (i, angle, axiom, rules) = expandAxiom (i-1, angle, replace (i, angle, axiom, rules), rules)
+expandAxiom ls@(i, angle, axiom, rules) = expandAxiom (i-1, angle, replace ls, rules)
+
+showExpandedAxim :: LSystem -> IO String
+showExpandedAxim ls@(i, angle, axiom, rules) = return (expandAxiom ls)
 
 -- our program counts the number of spaces in input.txt
 main :: IO ()
 main = do
     c <- getArgs
     n <- parseGrammarFile (head c)
-    print n
+    s <- parseGrammarData n
+    x <- showExpandedAxim s
+    print x
 
-    -- Render the grammar
-    display
-        (InWindow "Gloss Line"
-		(400, 150) 	 -- window size
-		(10, 10)) 	 -- window position
-	white			 -- background color
-	picture			 -- picture to display
-
-picture = line [
-    ( 50, 50),
-    ( 100, 100)]
+-- Render the grammar
+--  display
+--      (InWindow "Gloss Line"
+--      (400, 150)  -- window size
+--      (10, 10)) 	 -- window position
+--  white			 -- background color
+--  picture		 -- picture to display
+--
+-- picture = line [
+--  ( 50, 50),
+--  ( 100, 100)]
