8aaa15a10808ea24d30190c557c391967351560e
Lucas N. Ferreira <lferreira@ucsc.edu>
2016-03-01 13:56:46 -0800

    Started implementing rendering functions

--- lsystems.hs
+++ lsystems.hs
@@ -1,4 +1,14 @@
+--
+-- University of California, Santa Cruz --
+--  Jack Baskin School of Engineering   --
+--    Computer Science Department       --
+--   CMPS203 - Pragramming Languages    --
+
 -- Haskell L-System
+
+-- Authors: Lucas N. Ferreira - lferreria@ucsc.edu --
+--          Sumukh Atreya     - satreya@ucsc.edu   --
+
 import System.Environment
 import Graphics.Gloss
 import Data.List.Split
@@ -6,6 +16,8 @@ import Data.List.Split
 -- data types
 type Rule     = (Char, String)
 type LSystem  = (Int, Float, String, [Rule])
+
+type FloatVector2  = (Float, Float)
 
 consts :: String
 consts = ['+', '-']
@@ -58,6 +70,7 @@ parseGrammarFile fileName = do
   text <- readFile fileName
   return (getGrammarLine text)
 
+-- LSystems implementation
 execRule :: Char -> Rule -> String
 execRule x (pre, sec) = if x == pre then sec else []
 
@@ -71,19 +84,34 @@ expandAxiom :: LSystem -> String
 expandAxiom (0, _ , axiom, _) = axiom
 expandAxiom ls@(i, angle, axiom, rules) = expandAxiom (i-1, angle, replace ls, rules)
 
-showExpandedAxim :: LSystem -> IO String
-showExpandedAxim ls@(i, angle, axiom, rules) = return (expandAxiom ls)
+showExpandedAxiom :: LSystem -> IO String
+showExpandedAxiom ls@(i, angle, axiom, rules) = return (expandAxiom ls)
 
--- our program counts the number of spaces in input.txt
+-- Rendering functions
+-- nextLinePoint :: FloatVector2 -> Float -> FloatVector2
+-- nextLinePoint curPoint dir = ((fst curPoint) + 10 * dir, (snd curPoint) + 10 * dir)
+--
+-- renderExpandedAxiom :: String -> [FloatVector2]
+-- renderExpandedAxiom [] = []
+-- renderExpandedAxiom (x:xs) = [nextLinePoint x] ++ (renderExpandedAxiom xs)
+
 main :: IO ()
 main = do
-    c <- getArgs
-    n <- parseGrammarFile (head c)
-    s <- parseGrammarData n
-    x <- showExpandedAxim s
+
+    -- Get the grammar filepath as an command line argument
+    commandLineArgs <- getArgs
+
+    -- Parse grammar file into a list of strings
+    grammarData <- parseGrammarFile (head commandLineArgs)
+
+    -- Create an LSystem using the grammar data described in the file
+    lsystem <- parseGrammarData grammarData
+
+    -- Apply LSystem expansion rules to get the result
+    x <- showExpandedAxiom lsystem
     print x
 
--- Render the grammar
+    -- Render the results
 --  display
 --      (InWindow "Gloss Line"
 --      (400, 150)  -- window size
